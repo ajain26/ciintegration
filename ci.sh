940a44274b74b4dcd8a75fd78e001429374b151b
ajain26 <anshul.jainsm@gmail.com>
2019-11-20 22:56:01 +0530

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ser -e
+set -e
 xcodebuild -project xcodebuild -project 'Numero.xcodeproj'  -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8'
 xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
